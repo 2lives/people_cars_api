d3d2617c83c7aadea940a7e6b0cd05d588f7d3fd
2lives <brandon.slipjet@gmail.com>
2020-11-18 00:19:07 -0800

    Changed seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,10 @@
 require 'faker'
 # Create 100 people
-(0..100).each do
+(0..30).each do
     Person.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, email: Faker::Internet.email)
 end
-Car.create(year: 2_020, make: 'Tesla', model: 'P100D', price: 100_000)
-Car.create(year: 2_016, make: 'Honda', model: 'Civic', price: 20_000)
+
+Car.create(year: 2_020, make: 'Tesla', model: 'P100D', price: 100_000, person_id: 1)
+Car.create(year: 2_016, make: 'Honda', model: 'Civic', price: 20_000, person_id: 29)
+Car.create(year: 2_016, make: 'Toyota', model: 'Camry', price: 10_000, person_id: 15)
+Car.create(year: 2_016, make: 'Lamborghini', model: 'Aventador', price: 400_000, person_id: 3)
